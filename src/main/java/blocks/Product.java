13c248c11925b6a044bdfa65a3c4f865125ca3bb
Inna <innasamus@gmail.com>
2020-06-22 22:29:52 +0300

    atlas - 3

--- src/main/java/blocks/Product.java
+++ src/main/java/blocks/Product.java
@@ -1,6 +1,9 @@
 package blocks;
 
 import io.qameta.atlas.webdriver.AtlasWebElement;
+import io.qameta.atlas.webdriver.WebPage;
+import io.qameta.atlas.webdriver.extension.FindBy;
+import io.qameta.atlas.webdriver.ElementsCollection;
 import io.qameta.atlas.webdriver.extension.FindBy;
 
 public interface Product extends AtlasWebElement<Product> {
